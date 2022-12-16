014a5f8bd54391acaff692a84f8e68f5226c47b7
Rohit Yadav <rohit.yadav@shapeblue.com>
2022-12-16 17:39:50 +0530

    set next version for 4.18
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- source/conf.py
+++ source/conf.py
@@ -24,9 +24,9 @@ copyright = '2012-2022, Apache Foundation'
 author = 'Apache CloudStack Project'
 
 # The short X.Y version
-version = '4.17'
+version = '4.18'
 # The full version, including alpha/beta/rc tags
-release = '4.17.2.0'
+release = '4.18.0.0'
 
 rst_epilog = """
 .. include:: /_global.rst 
