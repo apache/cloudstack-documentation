f0be85c832d83d07f7b1331955ed640339d05685
Daan Hoogland <daan@onecht.net>
2023-02-23 15:11:41 +0100

    sphinx.ext.autosectionlabel for cross file reference

--- source/conf.py
+++ source/conf.py
@@ -45,6 +45,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
+    'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
