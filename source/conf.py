c0ad4c6190a13471157cda9f6c1d08969d4f0a0f
Rohit Yadav <rohit.yadav@shapeblue.com>
2023-05-12 12:47:40 +0530

    source: add sphnix-tabs
    These could be useful in the quick install guide for example which is
    hard to follow and group tabs could be usefule:
    
    https://sphinx-tabs.readthedocs.io/en/latest/#group-tabs
    
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- source/conf.py
+++ source/conf.py
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.autosectionlabel',
+    'sphinx_tabs.tabs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
