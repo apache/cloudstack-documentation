ba25e14ff4ead8b3a012f4aa31f96529e5b60bae
Rohit Yadav <rohit.yadav@shapeblue.com>
2024-04-03 23:29:48 +0530

    source: bump version to 4.18.1.1
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- source/conf.py
+++ source/conf.py
@@ -26,7 +26,7 @@ author = 'Apache CloudStack Project'
 # The short X.Y version
 version = '4.18'
 # The full version, including alpha/beta/rc tags
-release = '4.18.1.0'
+release = '4.18.1.1'
 
 rst_epilog = """
 .. include:: /_global.rst 
